862ede233754133222877f0005e286762d6d8eb4
rhino-arch <ngochungwru@gmail.com>
2025-08-10 20:07:08 +0700

    Add files via upload

---
+++ datatree/createTree2.py
@@ -0,0 +1,15 @@
+from Grasshopper import DataTree
+from Grasshopper.Kernel.Data import GH_Path
+from Rhino.Geometry import Point3d
+
+def createTreee(points, breakP):
+    numPath = 0
+    newPath = GH_Path(0)
+    tree = DataTree[Point3d]()
+    
+    for num in range(len(points)):
+        if num % breakP == 0 and num != 0:
+            numPath += 1
+            newPath = GH_Path(numPath)
+        tree.Add(points[num], newPath)
+    return tree
